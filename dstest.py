64021587cf0e28c92c38b1e0d62f42842a446d79
j <coffeesnakes@gmail.com>
2022-05-09 21:37:49 -0700

    dfstest

---
+++ dstest.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+
+			array.append(self.name)
+			for child in self.children:
+				print(self.name)
+				child.depthFirstSearch(array)
+			return array
+
+
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        graph = program.Node("A")
+        graph.addChild("B").addChild("C").addChild("D")
+        graph.children[0].addChild("E").addChild("F")
+        graph.children[2].addChild("G").addChild("H")
+        graph.children[0].children[1].addChild("I").addChild("J")
+        graph.children[2].children[0].addChild("K")
+        self.assertEqual(graph.depthFirstSearch([]), ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"])
