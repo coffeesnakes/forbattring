26df437a643a5c297a5e4c9bc1dc767a97e87b6f
j <coffeesnakes@gmail.com>
2022-10-10 16:52:46 -0700

    noteB

---
+++ shoot.py
@@ -0,0 +1,54 @@
+txt = "Hello my FRIENDS"
+
+x = txt.isalnum()
+
+print(x)
+
+##hashmap
+# map = {'lebron': 8, 'kobe': 24}
+# print(map['kobe'])
+# mappy = {i: i*3 for i in range(100)}
+# print(mappy)
+# print(mappy[60])
+# print(3*60)
+# mappy['hfp'] = 'jake'
+# print(mappy['hfp'])
+
+
+
+
+##hashset has only unique values 
+# hash = set()
+# hash.add(1)
+# hash.add(33)
+# hash.add(33*2)
+# print(hash)
+# hash.remove(66)
+# print(66 in hash)
+# print(0 in hash)
+# hash = set()
+
+
+
+
+
+
+## sublists/slicing
+
+# arr = ['darius', 'olga', 'angel', 'aa']
+
+# arr.sort(key=lambda x: len(x))
+# print(arr)
+
+# test = [i for i in range(101)]
+# print(test)
+
+## 2d array create
+
+# arr = [[1, 0] * 2 for i in range(4)]
+# print(arr)
+# from collections import deque
+
+# arr = [1, 2, 3]
+# arr.popleft()
+# print(arr)
